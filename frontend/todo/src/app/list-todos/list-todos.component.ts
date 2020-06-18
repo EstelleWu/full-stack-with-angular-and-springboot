21f0b6351dc2637c0bf94278aa838ee22d39b5dd
Estelle <EstelleWu@git.com>
2020-06-18 12:45:30 -0700

    create todo class and reformat todo table

--- frontend/todo/src/app/list-todos/list-todos.component.ts
+++ frontend/todo/src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Todo {
+  constructor(
+    public id: number,
+    public description: string,
+    public done: boolean,
+    public targetDate: Date
+  ){
+
+  }
+}
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -7,11 +18,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListTodosComponent implements OnInit {
   todos = [
-    {id: 1, description: 'Learn to Dance'},
-    {id: 2, description: 'Become an Expert at Angular'},
-    {id: 3, description: 'Visit Dongshan Road'}
+    new Todo(1, 'Learn to Dance', false, new Date()),
+    new Todo(2, 'Become an Expert at Angular', false, new Date()),
+    new Todo(3, 'Visit Dongshan Road', false, new Date()),
   ]
-  
+
   constructor() { }
 
   ngOnInit(): void {
