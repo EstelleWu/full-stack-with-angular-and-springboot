991e727a2f6626c175748e7d6ec8d2b16583157e
Estelle <EstelleWu@git.com>
2020-06-17 23:00:14 -0700

    event binding and data binding regarding login component

--- frontend/todo/src/app/login/login.component.ts
+++ frontend/todo/src/app/login/login.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  username = 'qiugaoqishuang'
+  password = ''
+  errorMessage = " invalid credential"
+  invalidLogin = false
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  handleLogin(){
+
+    console.log(this.username);
+    if (this.username === 'qiugaoqishuang' && this.password === 'dummy'){
+      this.invalidLogin = false;
+    }
+  }
 }
