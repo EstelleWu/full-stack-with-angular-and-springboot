5c7db485be6467f4884e99bf45c45de288910fe8
Estelle <EstelleWu@git.com>
2020-06-18 19:43:40 -0700

    run with rest and spring boot

---
+++ restful-web-services/src/test/java/com/qiyu/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qiyu.rest.webservices.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
