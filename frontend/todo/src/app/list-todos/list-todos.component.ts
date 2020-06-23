70f6b0ecd78b51ad9e0a778469be2d09473c6ec7
Estelle <EstelleWu@git.com>
2020-06-22 22:57:02 -0700

    connected angular front end with todo list restful services

--- frontend/todo/src/app/list-todos/list-todos.component.ts
+++ frontend/todo/src/app/list-todos/list-todos.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoDataService } from '../service/data/todo-data.service';
 
 export class Todo {
   constructor(
     public id: number,
     public description: string,
-    public done: boolean,
+    public completed: boolean,
     public targetDate: Date
   ){
 
@@ -17,15 +18,24 @@ export class Todo {
   styleUrls: ['./list-todos.component.css']
 })
 export class ListTodosComponent implements OnInit {
-  todos = [
-    new Todo(1, 'Learn to Dance', false, new Date()),
-    new Todo(2, 'Become an Expert at Angular', false, new Date()),
-    new Todo(3, 'Visit Dongshan Road', false, new Date()),
-  ]
+  todos: Todo[]
+  // [
+  //   new Todo(1, 'Learn to Dance', false, new Date()),
+  //   new Todo(2, 'Become an Expert at Angular', false, new Date()),
+  //   new Todo(3, 'Visit Dongshan Road', false, new Date()),
+  // ]
 
-  constructor() { }
+  constructor(
+    private todoService: TodoDataService
+  ) { }
 
   ngOnInit(): void {
+    this.todoService.retrieveAllTodos('qiugaoqishuang').subscribe(
+      response => {
+        console.log(response);
+        this.todos = response;
+      }
+    );
   }
 
 }
